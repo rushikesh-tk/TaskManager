4af0cfd6c50631e856dcdf1f14686b979303844a
Rushikesh Taksande <rushikeshtaksande7@gmail.com>
2024-09-19 08:18:36 +0530

    server integration done

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,52 @@
-console.log("Hiiiii========");
+import express from "express";
+import dotenv from "dotenv";
+import connectDB from "./config/db.js";
+import userRoutes from "./routes/userRoutes.js";
+import todoRoutes from "./routes/todoRoutes.js";
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
+import cors from "cors";
+import oauth2Client from "./utils/oauth2Client.js";
+
+// export NODE_TLS_REJECT_UNAUTHORIZED='0'
+// echo $NODE_TLS_REJECT_UNAUTHORIZED   // when google login does not work
+
+const PORT = process.env.PORT || 5000;
+
+dotenv.config();
+
+connectDB();
+
+const app = express();
+
+app.use(express.json());
+
+// const corsOptions = {
+//   origin: "https://trelloapp-eight.vercel.app",
+//   methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+//   credentials: true,
+// }; // For production
+
+const corsOptions = {
+  origin: "http://localhost:3000",
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  credentials: true,
+}; // For development
+
+app.options("*", cors(corsOptions)); // Preflight handling
+
+app.use(cors(corsOptions));
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Headers", "*");
+  next();
+});
+
+app.use("/api/users", userRoutes);
+app.use("/api/todos", todoRoutes);
+
+app.use(notFound);
+app.use(errorHandler);
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port : ${PORT}`);
+});
