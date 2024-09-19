b903d36ebfb78370336160828414076e049d441c
Rushikesh Taksande <rushikeshtaksande7@gmail.com>
2024-09-19 08:48:43 +0530

    const files added

---
+++ frontend/src/constants/userConst.js
@@ -0,0 +1,15 @@
+export const USER_REGISTER_REQUEST = "USER_REGISTER_REQUEST";
+export const USER_REGISTER_SUCCESS = "USER_REGISTER_SUCCESS";
+export const USER_REGISTER_FAIL = "USER_REGISTER_FAIL";
+
+export const USER_LOGIN_REQUEST = "USER_LOGIN_REQUEST";
+export const USER_LOGIN_SUCCESS = "USER_LOGIN_SUCCESS";
+export const USER_LOGIN_FAIL = "USER_LOGIN_FAIL";
+export const USER_LOGOUT = "USER_LOGOUT";
+
+export const USER_DATA_REQUEST = "USER_DATA_REQUEST";
+export const USER_DATA_SUCCESS = "USER_DATA_SUCCESS";
+export const USER_DATA_FAIL = "USER_DATA_FAIL";
+
+export const API_BASE_URL = "http://localhost:5000"; // For development
+// export const API_BASE_URL = "https://trello-pt9r.onrender.com"; // For production
